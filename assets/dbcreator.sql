45bf79cefc90d7fc121dc745bbc3731264d431c3
Maciek <moroz.maciek@gmail.com>
2025-06-21 08:40:14 +0200

    sync

--- assets/dbcreator.sql
+++ assets/dbcreator.sql
@@ -1,5 +1,10 @@
-update results set total_questions = 2 where quiz_id = 1;
-update results set total_questions = 2 where quiz_id = 10;
-update results set total_questions = 1 where quiz_id = 14;
-update results set total_questions = 1 where quiz_id = 16;
-update results set total_questions = 2 where quiz_id = 17;
+create table recepies (id INT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(255),
+                       instructions TEXT
+);
+CREATE TABLE ingredients_recepies (
+                             id INT AUTO_INCREMENT PRIMARY KEY,
+                             recepie_id INT NOT NULL,
+                             ingredient VARCHAR(255),
+                             FOREIGN KEY (recepie_id) REFERENCES recepies(id) ON DELETE CASCADE
+);
